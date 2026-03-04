d0b47e2444b2b37871b8fdbe7299101fef3abcfe
Tahaa-Dev <tahamahmoud7097@gmail.com>
2026-03-05 01:07:38 +0200

    Improvement: Improve Error struct

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,17 @@
+use std::{num::{IntErrorKind, ParseIntError}, str::ParseBoolError};
+
 #[derive(Debug)]
-pub enum LexError {
+pub enum Error {
     InvalidCommand(String),
-    UnclosedParentheses,
     InvalidType(String),
     UnexpectedToken(String, String),
     UnexpectedEOF,
-    ParseError(String),
+    ParseFailure(String),
 }
 
-impl std::error::Error for LexError {}
+impl std::error::Error for Error {}
 
-impl std::fmt::Display for LexError {
+impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::InvalidCommand(cmd) => {
@@ -20,18 +21,74 @@ impl std::fmt::Display for LexError {
                     cmd
                 )
             }
-            Self::UnclosedParentheses => {
-                write!(
-                    f,
-                    "Invalid `CREATE` statement\nFound unclosed parentheses"
-                )
-            }
             Self::InvalidType(ty) => write!(f, "Invalid type: {}", ty),
             Self::UnexpectedToken(untoken, token) => {
                 write!(f, "Unexpected token: {}\nExpected: {}", untoken, token)
             }
-            Self::UnexpectedEOF => write!(f, "Unexpected EOF"),
-            Self::ParseError(s) => write!(f, "Failed to parse: {}", s),
+            Self::UnexpectedEOF => f.write_str("Unexpected EOF"),
+            Self::ParseFailure(s) => write!(f, "Failed to parse: {}", s),
         }
+    }
+}
+
+impl<'a> From<nom::Err<nom::error::Error<&'a str>>> for Error {
+    fn from(e: nom::Err<nom::error::Error<&'a str>>) -> Self {
+        match e {
+            nom::Err::Incomplete(_) => Error::UnexpectedEOF,
+
+            nom::Err::Error(e) | nom::Err::Failure(e) => {
+                let input = e.input.to_string();
+                match e.code {
+                    nom::error::ErrorKind::MultiSpace
+                    | nom::error::ErrorKind::Space
+                    | nom::error::ErrorKind::TakeWhile1 => Error::UnexpectedEOF,
+
+                    nom::error::ErrorKind::Eof => Error::UnexpectedEOF,
+
+                    nom::error::ErrorKind::Tag => {
+                        Error::UnexpectedToken(input, "<KEYWORD>".into())
+                    }
+
+                    nom::error::ErrorKind::IsNot => Error::InvalidType(input),
+
+                    nom::error::ErrorKind::Count => Error::ParseFailure(input),
+
+                    nom::error::ErrorKind::ManyTill => {
+                        Error::InvalidType(input)
+                    }
+
+                    nom::error::ErrorKind::NoneOf => {
+                        Error::InvalidType(input)
+                    }
+
+                    nom::error::ErrorKind::AlphaNumeric
+                    | nom::error::ErrorKind::Alpha => {
+                        Error::UnexpectedToken(input, "<IDENTIFIER>".into())
+                    }
+
+                    nom::error::ErrorKind::Digit => {
+                        Error::UnexpectedToken(input, "<NUMBER>".into())
+                    }
+
+                    _ => Error::ParseFailure(input),
+                }
+            }
+        }
+    }
+}
+
+impl From<ParseIntError> for Error {
+    fn from(value: ParseIntError) -> Self {
+        match value.kind() {
+            IntErrorKind::Empty => Error::ParseFailure("".into()),
+            IntErrorKind::Zero => Error::ParseFailure("0".into()),
+            _ => Error::ParseFailure("Invalid number".into()),
+        }
+    }
+}
+
+impl From<ParseBoolError> for Error {
+    fn from(value: ParseBoolError) -> Self {
+        Error::ParseFailure("Non bool".into())
     }
 }
